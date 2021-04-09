f95da2ee2e3df6c46ecccd0bf55fca9a18ab920e
storm.witziers <storm.witziers@basebuilder.com>
2021-04-10 01:32:26 +0200

    Player doesnt die after game over

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,26 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private string _name = "Player";
+    
+    private bool _alive;
+    private bool _gameOver = false;
     public string Name
     {
         get => _name;
         set => _name = value;
     }
-
-    private bool _alive;
     public bool Alive
     {
         get => _alive;
-        set
-        {
-            _alive = value;
-        }
+        set => _alive = value;
     }
-
+    
+    
     public void StartGameInput(InputAction.CallbackContext context)
     {
         GameController.INSTANCE.StartGameInput();
@@ -31,10 +31,23 @@ public class Player : MonoBehaviour
         GetComponent<PlayerTemperature>().OnPlayerDied(PlayerDied);
     }
 
+    public void DisableTemperature()
+    {
+        var temperature = GetComponent<PlayerTemperature>();
+        temperature.GameStarted = false;
+    }
+    
+    public void EnableTemperature()
+    {
+        var temperature = GetComponent<PlayerTemperature>();
+        temperature.GameStarted = true;
+        temperature.ResetValues();
+    }
+    
     private void PlayerDied(string name)
     {
         Debug.Log(name + " Died!");
         _alive = false;
-        Destroy(gameObject);
+        gameObject.SetActive(false);
     }
 }
