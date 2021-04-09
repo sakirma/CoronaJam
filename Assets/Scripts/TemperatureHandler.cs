70c07cb2039c7eee717e98d9d771b36f4c4e3b1d
storm.witziers <storm.witziers@basebuilder.com>
2021-04-09 20:15:44 +0200

    Debuff fix

--- Assets/Scripts/TemperatureHandler.cs
+++ Assets/Scripts/TemperatureHandler.cs
@@ -71,7 +71,7 @@ public class TemperatureHandler : MonoBehaviour
         using var positionKeys = _positions.Keys.GetEnumerator();
         using var debuffKeys = _debuffs.Keys.GetEnumerator();
 
-        var debuff = 0;
+        var debuff = 1;
         
         var changed = false;
         var rankedPositions = _distances.OrderBy(x => x.Value).ToList();
@@ -83,7 +83,7 @@ public class TemperatureHandler : MonoBehaviour
                 _debuffs[e.Key] = debuff;
                 changed = true;
             }
-            debuff -= 1;
+            debuff -= 2;
         });
         
         return changed;
